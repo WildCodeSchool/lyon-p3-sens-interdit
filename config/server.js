13482a47ec7de4a0933dd4f74249dbfc6954400f
Seblecaribou <morin.seb.89@gmail.com>
2020-10-07 14:11:21 +0200

    create-strapi-app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '57924e43161370757998b702610fd6cf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c578164a-8f0c-4247-964b-76d4d23468bf'
+};
