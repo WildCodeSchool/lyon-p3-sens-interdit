a6b0249a656a7b8417827e8837c6572d18849d74
Marine Montailler <marinemontailler@gmail.com>
2020-10-22 16:11:52 +0200

    add img, styles, and html content in header component

--- FRONT/gatsby-sens-interdits/src/pages/index.js
+++ FRONT/gatsby-sens-interdits/src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
+import Header from "../components/globalscomponents/Header"
+import '../styles/global.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className="body">
+    <Header />
+  </div>
 }
