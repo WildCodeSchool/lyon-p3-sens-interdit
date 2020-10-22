abbc95a899ca8c67d830336ae4ad86b8cfe3f12e
Marine Montailler <marinemontailler@gmail.com>
2020-10-22 16:51:42 +0200

    add footer in react component

--- FRONT/gatsby-sens-interdits/src/pages/index.js
+++ FRONT/gatsby-sens-interdits/src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
-import Header from "../components/globalscomponents/Header"
+import Footer from "../components/globalscomponents/footercomponents/Footer"
+import Header from "../components/globalscomponents/headercomponents/Header"
 import '../styles/global.css'
 
 export default function Home() {
   return <div className="body">
     <Header />
+    <Footer />
   </div>
 }
