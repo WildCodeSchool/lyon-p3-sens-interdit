4231d4e9c3c2cd50331ba774a8a3622f1649924b
Marine Montailler <marinemontailler@gmail.com>
2020-10-23 11:41:43 +0200

    add 404 page

---
+++ FRONT/gatsby-sens-interdits/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const NotFoundPage = () => (
+  <div>
+    <h1>404: Not Found</h1>
+    <p>You just hit a route that doesn't exist... the sadness.</p>
+  </div>
+)
+
+export default NotFoundPage
