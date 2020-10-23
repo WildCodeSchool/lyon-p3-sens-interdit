1da9ca5f31df40a7bf32274e2d4c4d958fe2b32e
Marine Montailler <marinemontailler@gmail.com>
2020-10-23 11:16:02 +0200

    create components in header : edition, fr-en, logo, navbar, newsletterBtn, searchBar, socialNetwork, Ticketing

---
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/headercomponents/FrEn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FrEn(){
+    return(
+        <div class="fr-en">
+            <p>F R</p>
+            <span> | </span>
+            <p>E N</p>
+          </div>
+    )
+}
---
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/headercomponents/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import senslogo from '../../../img/logosens.svg';
+
+
+export default function Logo(){
+    return(
+        <div class="main-logo">
+            <img src={senslogo} alt="logo du festival" />
+        </div>
+    )
+}
