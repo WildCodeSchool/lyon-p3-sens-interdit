28ffb37d75e3930e7a19d8310546525a6f8f3107
Marine Montailler <marinemontailler@gmail.com>
2020-11-04 16:17:12 +0100

    add content in main index page

--- FRONT/gatsby-sens-interdits/src/pages/404.js
+++ FRONT/gatsby-sens-interdits/src/pages/404.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Layout from "../components/globalscomponents/Layout"
+import {Link} from "gatsby"
 
 const NotFoundPage = () => (
-  <div>
+  <Layout>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn't exist... the sadness.</p>
-  </div>
+    <Link to="/">Go back to Home Page</Link>
+  </Layout>
 )
 
 export default NotFoundPage
