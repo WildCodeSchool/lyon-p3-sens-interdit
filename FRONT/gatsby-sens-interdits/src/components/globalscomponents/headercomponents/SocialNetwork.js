1b240c808e7fffcd5937ebae86afdb47d2d124e5
Marine Montailler <marinemontailler@gmail.com>
2020-11-04 11:42:35 +0100

    add link in each socialmedia

--- FRONT/gatsby-sens-interdits/src/components/globalscomponents/headercomponents/SocialNetwork.js
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/headercomponents/SocialNetwork.js
@@ -2,37 +2,38 @@ import React from 'react';
 import facebook from '../../../img/facebook.svg';
 import insta from '../../../img/insta.svg';
 import twitter from '../../../img/twitter.svg';
+import radio from '../../../img/radio.svg'
 import share from '../../../img/share.svg';
 
 export default function SocialNetwork(){
     return(
         <ul class="social-network">
-            <li><a href="#" title="link to facebook" target="_blank"
+            <li><a href="https://www.facebook.com/sens.interdits/" title="link to facebook" target="_blank"
               ><img
                 src={facebook}
                 alt="facebook"
-                width="80"
+                width="30"
                 style={{filter: "invert(100%)"}}
             /></a></li>
-            <li><a href="#" title="link to instagram" target="_blank"
+            <li><a href="https://www.instagram.com/sensinterdits/" title="link to instagram" target="_blank"
               ><img
                 src={insta}
                 alt="instagram"
                 style={{filter: "invert(100%)"}}
-                width="80"
+                width="30"
             /></a></li>
-            <li><a href="#" title="link to twitter" target="_blank"
+            <li><a href="https://twitter.com/sens_interdits?lang=fr" title="link to twitter" target="_blank"
               ><img
                 src={twitter}
                 alt="twitter"
                 style={{filter: "invert(100%)"}}
-                width="80"
+                width="30"
             /></a></li>
-            <li><a href="#" title="link to linkedin" target="_blank"
+            <li><a href="#" title="link to radio" target="_blank"
               ><img
-                src={share}
-                alt="linkedin"
-                width="80"
+                src={radio}
+                alt="radio"
+                width="30"
                 style={{filter: "invert(100%)"}}
             /></a></li>
             <li><a href="#" title="link to share" target="_blank"
@@ -40,7 +41,7 @@ export default function SocialNetwork(){
                 src={share}
                 alt="share"
                 style={{filter: "invert(100%)"}}
-                width="80"
+                width="30"
             /></a></li>
           </ul>
     )
--- FRONT/gatsby-sens-interdits/src/pages/index.js
+++ FRONT/gatsby-sens-interdits/src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Footer from "../components/globalscomponents/footercomponents/Footer"
 import Header from "../components/globalscomponents/headercomponents/Header"
-import '../styles/global.css'
+import '../styles/header-footer.css'
 
 export default function Home() {
   return <div className="body">
