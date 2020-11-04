9773acaae724b4c347823f70126a1493c2016e12
Marine Montailler <marinemontailler@gmail.com>
2020-11-04 13:04:30 +0100

    make html footer in 4 composants

--- FRONT/gatsby-sens-interdits/src/components/globalscomponents/footercomponents/Footer.js
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/footercomponents/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import senslogo from '../../../img/logosens.svg';
-import celestins from '../../../img/logo_celestins.png';
-import logolyon from '../../../img/logo_lyon.png';
-import logonova from '../../../img/logo_nova.jpeg';
-import logofemmes from '../../../img/logo_femmes.png';
-import logoara from '../../../img/logo_ARA.png'
+import Logo from '../headercomponents/Logo';
 import SocialNetwork from '../headercomponents/SocialNetwork';
+import AdressList from './AdressList';
+import ListLink from './ListLink';
+import Newsletter from './Newsletter';
 
 
 export default function Footer(){
@@ -14,41 +12,18 @@ export default function Footer(){
 <footer>
       <div class="wrapper-footer">
         <div class="footer-column1">
-          <div class="footer-column1-title">
-            <img src={senslogo} width="300" alt="logo partner2" />
-          </div>
+          <Logo/>
           <div class="footer-column1-socialnetwork">
             <p>Suivez-nous !</p>
             <SocialNetwork />
           </div>
-          <div class="footer-column1-listlink">
-            <ul>
-              <li><a>Contacts</a></li>
-              <li><a>Mentions Légales</a></li>
-              <li><a>Plan du site</a></li>
-            </ul>
-          </div>
+          <ListLink />
         </div>
         <div class="footer-column2">
-          <div class="footer-column2-newsletter">
-            <div class="newsletter-container">
-              <div>
-                <h3 style={{margin: "0"}}>NEWSLETTER</h3>
-                <p style={{margin: "0"}}>Recevez nos actualités</p>
-              </div>
-              <input type="text" />
-            </div>
-          </div>
-          <div class="footer-column1-listlink">
-            <ul>
-              <li>Association Sens Interdits 14, rue Basse Combalot 69007 Lyon, France</li>
-              <li>Tél : +33 (0)9 67 02 00 85</li>
-              <li>Mail : festivalsensinterdits@gmail.com</li>
-            </ul>
-          </div>
+        <Newsletter />
+        <AdressList />
         </div>
       </div>
-      
     </footer>
     )
 }
