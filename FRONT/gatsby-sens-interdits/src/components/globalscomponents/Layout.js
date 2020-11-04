19136cff06c846cd42b14e68fc0f52f9e427efa8
justinehell <hell.justine@gmail.com>
2020-11-04 17:39:34 +0100

    gatsby - add wrapPageElement for global layout

--- FRONT/gatsby-sens-interdits/src/components/globalscomponents/Layout.js
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/Layout.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Footer from './footercomponents/Footer';
-import Header from './headercomponents/Header';
-import '../../styles/header-footer.css'
+import React from "react"
+import Footer from "./footercomponents/Footer"
+import Header from "./headercomponents/Header"
+import "../../styles/header-footer.css"
 
-
-export default function Layout({children}){
-    return(
-        <>
-            <Header/>
-            <main>{children}</main>
-            <Footer/>
-        </>
-    )
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
 }
--- FRONT/gatsby-sens-interdits/src/pages/404.js
+++ FRONT/gatsby-sens-interdits/src/pages/404.js
@@ -1,13 +1,12 @@
 import React from "react"
-import Layout from "../components/globalscomponents/Layout"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn't exist... the sadness.</p>
     <Link to="/">Go back to Home Page</Link>
-  </Layout>
+  </>
 )
 
 export default NotFoundPage
