47bb91e97e35ad9d3f23970f62cd185cfc9d177d
Marine Montailler <marinemontailler@gmail.com>
2020-11-04 16:17:12 +0100

    add content in main index page

--- FRONT/gatsby-sens-interdits/src/pages/index.js
+++ FRONT/gatsby-sens-interdits/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Layout from "../components/globalscomponents/Layout"
+import {Link} from "gatsby";
 
 export default function Home() {
   return <Layout>
     <h1>This is the home page</h1>
+    <Link to="/404">Go to 404 page</Link>
   </Layout>
 }
