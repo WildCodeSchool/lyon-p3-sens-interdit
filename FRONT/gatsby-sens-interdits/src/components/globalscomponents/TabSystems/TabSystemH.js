d76df787d064b9ac0cfcc032b25c4bbea7da109b
Seblecaribou <morin.seb.89@gmail.com>
2020-11-06 00:27:44 +0100

    Functioning tab system. Needs polish and refinement.

--- FRONT/gatsby-sens-interdits/src/components/globalscomponents/TabSystems/TabSystemH.js
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/TabSystems/TabSystemH.js
@@ -1,104 +1,188 @@
-//US : TabsystemGATSBY#65
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import picto from "../../../img/picto.svg"
+import "./tabsystem.css"
 
-import React from "react";
-import {useState} from "react";
-//design imports
-import picto from "../../../img/picto.svg";
-import "./tabsystem.css";
+function TabSystemH() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [contentTab, setContentTab] = useState([])
+  const [activeTabContent, setActiveTabContent] = useState("")
+  const [activeClass, setActiveClass] = useState("")
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:1337/spectacles")
+      .then(response => response.data)
+      .then(data => {
+        setContentTab(data[0].tab_element)
+        setActiveTabContent(data[0].tab_element[0].title)
+        setActiveClass(data[0].tab_element[0].title)
+        setIsLoading(false)
+      })
+  }, [])
 
+  function handleOnClick(e) {
+    setActiveTabContent(e.target.id)
+    setActiveClass(e.target.id)
+  }
 
-function TabSystemH (){
+  return isLoading ? (
+    <p>Loading please wait </p>
+  ) : (
+    <div className="tab-module">
+      {contentTab.map(tab => (
+        <>
+          <img
+            src={picto}
+            alt="pictogramme cliquable"
+            weight="30"
+            height="30"
+          />
+          <h3
+            id={tab.title}
+            className={
+              "tab-link" + " " + (activeClass === tab.title ? "active" : "")
+            }
+            id={tab.title}
+            onClick={handleOnClick}
+          >
+            {tab.title}
+          </h3>
+          <div
+            className={
+              activeTabContent === tab.title ? "active-tab" : "disabled-tab"
+            }
+          >
+            {tab.content}
+          </div>
+        </>
+      ))}
+    </div>
+  )
+  {
+    /* // <div className="tab-module">
+    //   <ul>
+    //     <li>
+    //       <a
+    //         href="#"
+    //         title=""
+    //         className={
+    //           "tab-link" + " " + (activeClass === "about" ? "active" : "")
+    //         }
+    //         id="about"
+    //         onClick={handleOnClick}
+    //       >
+    //         <img
+    //           src={picto}
+    //           alt="pictogramme cliquable"
+    //           weight="30"
+    //           height="30"
+    //         />
+    //         À propos
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         href="#"
+    //         title=""
+    //         className={
+    //           "tab-link" + " " + (activeClass === "biography" ? "active" : "")
+    //         }
+    //         id="biography"
+    //         onClick={handleOnClick}
+    //       >
+    //         <img
+    //           src={picto}
+    //           alt="pictogramme cliquable"
+    //           weight="30"
+    //           height="30"
+    //         />
+    //         Biographie
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         href="#"
+    //         title=""
+    //         className={
+    //           "tab-link" +
+    //           " " +
+    //           (activeClass === "distribution" ? "active" : "")
+    //         }
+    //         id="distribution"
+    //         onClick={handleOnClick}
+    //       >
+    //         <img
+    //           src={picto}
+    //           alt="pictogramme cliquable"
+    //           weight="30"
+    //           height="30"
+    //         />
+    //         Distribution
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         href="#"
+    //         title=""
+    //         className={
+    //           "tab-link" + " " + (activeClass === "press" ? "active" : "")
+    //         }
+    //         id="press"
+    //         onClick={handleOnClick}
+    //       >
+    //         <img
+    //           src={picto}
+    //           alt="pictogramme cliquable"
+    //           weight="30"
+    //           height="30"
+    //         />
+    //         Dans la press
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         href="#"
+    //         title=""
+    //         className={
+    //           "tab-link" + " " + (activeClass === "picturial" ? "active" : "")
+    //         }
+    //         id="picturial"
+    //         onClick={handleOnClick}
+    //       >
+    //         <img
+    //           src={picto}
+    //           alt="pictogramme cliquable"
+    //           weight="30"
+    //           height="30"
+    //         />
+    //         En image
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a
+    //         href="#"
+    //         title=""
+    //         className={
+    //           "tab-link" + " " + (activeClass === "withPublic" ? "active" : "")
+    //         }
+    //         id="withPublic"
+    //         onClick={handleOnClick}
+    //       >
+    //         <img
+    //           src={picto}
+    //           alt="pictogramme cliquable"
+    //           weight="30"
+    //           height="30"
+    //         />
+    //         Avec les publics
+    //       </a>
+    //     </li>
+    //   </ul>
 
-    const [contentTab,setContentTab]= useState ('about');
-    const [activeClass,setActiveClass]= useState('about');
-    console.log("active class", activeClass)
-    console.log("content tab", contentTab)
-
-    function handleOnClick(e){
-        setContentTab(e.target.id);
-        setActiveClass(e.target.id);
-    };
-
-    function displayContentTab (){
-        console.log ('content tab in function display', contentTab);
-        if (contentTab==="about"){
-            return(
-                <div>
-                    <p>Content = affiche le contenu about </p>
-                </div>
-            )
-        }else if (contentTab==="biography") {
-            return (
-                <div>
-                    <p>Content = affiche le contenu biography</p>
-                </div>
-            )
-        }else if (contentTab==="distribution") {
-            return (
-                <div>
-                    <p>Content = affiche le contenu distribution</p>
-                </div>
-            )
-        }else if (contentTab==="press") {
-            return (
-                <div> 
-                    <p>Content = affiche le contenu press</p>
-                </div>
-                )
-        }else if (contentTab==="picturial") {
-            return (
-                <div>
-                    <p>Content = affiche le contenu picturial</p>
-                </div>
-                )
-        }else if (contentTab==="withPublic") {
-            return (
-                <div>
-                    <p>Content = affiche le contenu with publics </p>
-                </div>
-                )
-        }else {
-            return <p>Error</p>
-        }
-    }
-
-    return (
-        <div className="tab-module">
-            <ul>
-                <li>
-                    <a href="#" title="" className={"tab-link" + " " + (activeClass === 'about' ? 'active' : '')} id="about" onClick={handleOnClick} >
-                    <img src={picto} alt="pictogramme cliquable" weight="30" height='30' />À propos</a>
-                </li>
-                <li>
-                    <a href="#" title="" className={"tab-link" + " " + (activeClass === 'biography' ? 'active' : '')} id="biography" onClick={handleOnClick}>
-                    <img src={picto} alt="pictogramme cliquable" weight="30" height='30' />Biographie</a>
-                </li>
-                <li>
-                    <a href="#" title="" className={"tab-link" + " " + (activeClass === 'distribution' ? 'active' : '')} id="distribution" onClick={handleOnClick}>
-                    <img src={picto} alt="pictogramme cliquable" weight="30" height='30' />Distribution</a>
-                </li>
-                <li>
-                    <a href="#" title="" className={"tab-link" + " " + (activeClass === 'press' ? 'active' : '')} id="press" onClick={handleOnClick}>
-                    <img src={picto} alt="pictogramme cliquable" weight="30" height='30' />Dans la press</a>
-                </li>
-                <li>
-                    <a href="#" title="" className={"tab-link" + " " + (activeClass === 'picturial' ? 'active' : '')} id="picturial" onClick={handleOnClick}>
-                    <img src={picto} alt="pictogramme cliquable" weight="30" height='30' />En image</a>
-                </li>
-                <li>
-                    <a href="#" title="" className={"tab-link" + " " + (activeClass === 'withPublic' ? 'active' : '')} id="withPublic" onClick={handleOnClick}>
-                    <img src={picto} alt="pictogramme cliquable" weight="30" height='30' />Avec les publics</a>
-                </li>
-            </ul>
-
-            <div>
-            {displayContentTab(contentTab)}
-            </div> 
-        </div>
-    )
+    //   <div>{displayContentTab(contentTab)}</div>
+    // </div> */
+  }
 }
 
-export default TabSystemH; 
-
-
+export default TabSystemH
