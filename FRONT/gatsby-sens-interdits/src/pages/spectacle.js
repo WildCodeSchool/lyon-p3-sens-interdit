7de9296199e24c77340e6c5f5a612638f8d71254
ShivanPaishari <shivan.paishari@yahoo.com>
2020-11-05 11:59:39 +0100

    adding tabsystem horizontal component

---
+++ FRONT/gatsby-sens-interdits/src/pages/spectacle.js
@@ -0,0 +1,14 @@
+import React from "react"
+import{TabSystemH} from "../components/globalscomponents/TabSystem/TabSystemH"
+
+const Spectacle = () => {
+  return (
+      <div>
+        <h1>This is the Spectacle page</h1>
+        <TabSystemH/>
+      </div>
+
+  )
+}
+
+export default Spectacle;
