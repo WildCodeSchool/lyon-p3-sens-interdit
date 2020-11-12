06880f78f9f5e7b5e813d5d15429c383c1ce8743
justinehell <hell.justine@gmail.com>
2020-11-12 11:21:31 +0100

    adding spectacle Page layout and Components

--- FRONT/gatsby-sens-interdits/src/pages/spectacle.js
+++ FRONT/gatsby-sens-interdits/src/pages/spectacle.js
@@ -1,11 +1,50 @@
 import React from "react"
-import ImageCarousel from "../components/globalscomponents/Carousel/ImageCarousel"
+import SpectacleInfos from "../components/globalscomponents/spectaclepagecomponents/SpectacleInfos"
 import TabSystemH from "../components/globalscomponents/TabSystems/TabSystemH"
+import Thumbnail from "../components/globalscomponents/Thumbnail/Thumbnail"
+import ImageCarousel from "../components/globalscomponents/Carousel/ImageCarousel"
+import CalendarLarge from "../components/globalscomponents/Calendar/CalendarLarge"
+
+import "./../components/globalscomponents/spectaclepagecomponents/SpectaclePage.css"
+
+import photoTest from "../img/img-sens-interdit.jpg"
+
 const Spectacle = () => {
   return (
     <div>
       <ImageCarousel />
-      <TabSystemH />
+
+      <div className="content-spectaclepage">
+        <div className="country-label">
+          <p>Kosovo</p>
+        </div>
+
+        <CalendarLarge />
+
+        <SpectacleInfos />
+
+        <TabSystemH />
+
+        <div className="content">
+          <p className="content-title">Autour du spectacle</p>
+          <div className="display-miniTab">
+            <Thumbnail
+              affiche={photoTest}
+              date="26 Octobre"
+              country="Russie"
+              name="Titre du spectacle"
+              team="Metteur en scène"
+            />
+            <Thumbnail
+              affiche={photoTest}
+              date="26 Octobre"
+              country="Russie"
+              name="Titre du spectacle"
+              team="Metteur en scène"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
