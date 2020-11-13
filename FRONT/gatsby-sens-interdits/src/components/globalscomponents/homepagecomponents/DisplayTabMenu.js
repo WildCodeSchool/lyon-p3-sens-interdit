3dd17903be9004ba4b54a0509aebde7d7354edd1
Seblecaribou <morin.seb.89@gmail.com>
2020-11-13 15:34:05 +0100

    WIP transmission page

--- FRONT/gatsby-sens-interdits/src/components/globalscomponents/homepagecomponents/DisplayTabMenu.js
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/homepagecomponents/DisplayTabMenu.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import picto from '../../../img/picto+.svg'
+import React from "react";
+import picto from "../../../img/picto+.svg";
 
-export default function DisplayTabMenu(props){
-    return(
-        <div className="display-tab-sticker">
-                    <img src={props.image} alt="sens" width="100%" height="100%"/>
-                    <div className="display-tab-title">
-                        <img className="display-tab-logo" src={picto} alt="plus" width="20"/>
-                        <p>{props.title}</p>
-                    </div>
-                </div>
-    )
+export default function DisplayTabMenu(props) {
+  return (
+    <div className="display-tab-sticker">
+      <img src={props.image} alt="sens" width="100%" height="100%" />
+      <div className="display-tab-title">
+        <a title={props.title} href="#">
+          <img className="display-tab-logo" src={picto} alt="plus" width="20" />
+        </a>
+        <p>{props.title}</p>
+      </div>
+    </div>
+  );
 }
