848d82c72b6d2739518162b8c00765c3d1c46f67
ShivanPaishari <shivan.paishari@yahoo.com>
2020-11-13 17:28:14 +0100

    structure without axios

--- FRONT/gatsby-sens-interdits/src/components/specifics/Festival/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Festival/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import CalendarLarge from "../Calendar/CalendarLarge";
-import DisplayTabMenu from "../homepagecomponents/DisplayTabMenu"
+import CalendarLarge from "../../globals/Calendar/CalendarLarge";
+import DisplayTabMenu from "../../globals/DisplayTabMenu/DisplayTabMenu";
 import "./festival.css";
 
 export default function festivalPage ()  {
@@ -17,25 +17,12 @@ export default function festivalPage ()  {
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
             <nav id="festival-menu">
-                <ul id="menu-container">
-                    <li >
-                        {festivalMenu.map(title=> 
+                {festivalMenu.map(title=> 
                                 <div>
                                     <DisplayTabMenu title={title}/>
                                 </div>
                             )
-                        }
-                    </li>
-                    {/* {for (let i=0 ; i<festivalMenu.length ; i++){
-                            return (
-                                <>
-                                    <li>
-                                        <DisplayTabMenu title= {festivalMenu.index[i]}
-                                    </li>
-                                </>
-                            )}
-                    } */}
-                </ul>
+                }
             </nav>
         </div>
     )
