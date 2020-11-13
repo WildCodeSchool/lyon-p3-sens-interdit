f5ccb93ea15fb17fa8bcfbe39def265bc1b378f4
Maxence <maxence.meloni@wildcodeschool.com>
2020-11-13 16:22:30 +0100

    rebase onto develop

--- FRONT/gatsby-sens-interdits/src/pages/transmissions.js
+++ FRONT/gatsby-sens-interdits/src/pages/transmissions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TransmissionsPage from "../components/globalscomponents/Transmission/TransmissionsPage";
+import TransmissionsPage from "../components/specifics/Transmission/";
 
 export default function Transmission() {
   return <TransmissionsPage />;
