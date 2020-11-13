b5fbe9d7a171adc64f3403a633154aedef3e4d48
ShivanPaishari <shivan.paishari@yahoo.com>
2020-11-13 15:36:58 +0100

    first step on festival page

---
+++ FRONT/gatsby-sens-interdits/src/pages/festival.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FestivalPage from "../components/globalscomponents/festival/festivalPage";
+
+export default function festival ()  {
+
+    return (
+    <>
+        <FestivalPage/>
+    </>
+    )
+}
