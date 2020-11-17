dca220741ec0969d163e4293a703e0516ab776c1
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-11-17 10:59:54 +0100

    clean code

--- FRONT/gatsby-sens-interdits/src/components/globalscomponents/programmepagecomponents/ProgrammePage.js
+++ FRONT/gatsby-sens-interdits/src/components/globalscomponents/programmepagecomponents/ProgrammePage.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react"
 import "./ProgrammePage.css"
 import photoTest from "../../../img/img-sens-interdit.jpg"
 import SpectacleMiniTab from "./SpectacleMiniTab"
-import FilterTab from "./FilterTab"
+//import FilterTab from "./FilterTab"
 import CalendarLarge from "./CalendarLarge"
+//import pouet from "../../../../public/"
+import "./FilterTab.css"
 
 export default function ProgrammePage(props) {
   const [list, setList] = useState(props.list)
@@ -11,6 +13,25 @@ export default function ProgrammePage(props) {
   useEffect(() => {
     setList(props.list)
   }, [props])
+
+  function countryFilter(e) {
+    e.preventDefault()
+    console.log("Le filtre pays a été cliqué.")
+  }
+
+  function authorFilter(e) {
+    e.preventDefault()
+    console.log("Le filtre metteur en scene a été cliqué.")
+  }
+
+  function placeFilter(e) {
+    e.preventDefault()
+    console.log("Le filtre lieu a été cliqué.")
+  }
+  function resetFilter(e) {
+    e.preventDefault()
+    console.log("Les filtres sont reinitialisés.")
+  }
 
   const affichageList = () => {
     console.log("AAAAAAAAAAAAAAAA", "list")
@@ -22,13 +43,20 @@ export default function ProgrammePage(props) {
         />
       )
     } else {
-      const date = "pouet"
       return list.map(spectacle => {
-        console.log(spectacle)
+        console.log(spectacle.all_photos.photo_1)
         return (
           <SpectacleMiniTab
-            key={spectacle.titre + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)}
-            affiche={photoTest}
+            key={
+              spectacle.titre +
+              Math.floor(Math.random() * 10) +
+              Math.floor(Math.random() * 10)
+            }
+            affiche={
+              spectacle.all_photos.photo_1 != undefined
+                ? spectacle.all_photos.photo_1
+                : photoTest
+            }
             date={
               spectacle.info_pratique.dates === undefined ||
               spectacle.info_pratique.dates.date_1 === undefined
@@ -61,8 +89,22 @@ export default function ProgrammePage(props) {
       </div>
       <div className="content-programmepage">
         <CalendarLarge />
-        <FilterTab />
-        <div className="display-miniTab">{affichageList(list)}</div>
+        <div className="global-FilterTab">
+          <h2> FILTREZ PAR: </h2>
+          <a href="#" onClick={countryFilter}>
+            <p> PAYS </p>
+          </a>
+          <a href="#" onClick={authorFilter}>
+            <p> METTEUR EN SCÈNE </p>
+          </a>
+          <a href="#" onClick={placeFilter}>
+            <p> LIEUX </p>
+          </a>
+          <a href="#" onClick={resetFilter}>
+            <h3> supprimer </h3>
+          </a>
+        </div>
+        <div className="display-miniTab"> {affichageList(list)} </div>
       </div>
     </div>
   )
