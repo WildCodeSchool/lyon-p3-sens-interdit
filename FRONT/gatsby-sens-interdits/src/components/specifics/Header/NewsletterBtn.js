c67d43201f5097d5580149db78b2496919a47bc8
Marine <marinemontailler@gmail.com>
2020-11-18 12:05:31 +0100

    change orthograph

--- FRONT/gatsby-sens-interdits/src/components/specifics/Header/NewsletterBtn.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Header/NewsletterBtn.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NewsletterBtn() {
     return (
         <div className="highlight newsletter-header to-uppercase">
-            <a href="#newsletter" title="ancor to newsletter">Newsletter</a>
+            <a href="#newsletter" title="anchor to newsletter">Newsletter</a>
         </div>
     )
 }
