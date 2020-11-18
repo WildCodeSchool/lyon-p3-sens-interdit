1d8fae8355a81f81b68e016e9fe154b51efb73c6
Marine <marinemontailler@gmail.com>
2020-11-18 11:40:16 +0100

    add carousel in festival

--- FRONT/gatsby-sens-interdits/src/components/specifics/Homepage/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Homepage/index.js
@@ -1,24 +1,19 @@
 import React from "react";
 import "./Index.css";
-import lefestival from "../../../assets/img/photosFiltre/lefestival.png";
 import DisplayTabMenu from "../../globals/DisplayTabMenu/DisplayTabMenu.js";
-import aveclespublics from "../../../assets/img/photosFiltre/aveclespublics.png";
-import spectaclesentournee from "../../../assets/img/photosFiltre/spectaclesentournee.png";
-import lassociation from "../../../assets/img/photosFiltre/lassociation.png";
-//import ImageCarousel from "../../globals/Carousel/ImageCarousel";
+import ImageCarousel from "../../globals/Carousel/ImageCarousel";
 
 export default function Homepage() {
   return (
     <div className="global-homepage">
-      {/* <ImageCarousel /> we need to pass props to this component */}
+      <ImageCarousel />
       <div className="content-homepage">
-        <DisplayTabMenu image={lefestival} title="Le Festival" />
-        <DisplayTabMenu image={aveclespublics} title="Avec les publics" />
+        <DisplayTabMenu title="Le Festival" />
+        <DisplayTabMenu title="Avec les publics" />
         <DisplayTabMenu
-          image={spectaclesentournee}
           title="Spectacles en tournée"
         />
-        <DisplayTabMenu image={lassociation} title="L'association'" />
+        <DisplayTabMenu title="L'association" />
       </div>
     </div>
   );
