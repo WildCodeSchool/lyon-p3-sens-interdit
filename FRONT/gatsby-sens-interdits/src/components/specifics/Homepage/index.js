4588bd03542b3c470633d36a224cd8aaab0a9bed
Justine <62500081+justinehell@users.noreply.github.com>
2020-11-18 09:50:51 +0100

    Update index.js

--- FRONT/gatsby-sens-interdits/src/components/specifics/Homepage/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Homepage/index.js
@@ -10,7 +10,7 @@ import lassociation from "../../../assets/img/photosFiltre/lassociation.png";
 export default function Homepage() {
   return (
     <div className="global-homepage">
-      {/* <ImageCarousel /> */} // we need to pass props to this component
+      {/* <ImageCarousel /> we need to pass props to this component */}
       <div className="content-homepage">
         <DisplayTabMenu image={lefestival} title="Le Festival" />
         <DisplayTabMenu image={aveclespublics} title="Avec les publics" />
