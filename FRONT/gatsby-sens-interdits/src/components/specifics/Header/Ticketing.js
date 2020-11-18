9487daba8d60b6c1a12b725f0c7e6d8e5de2c264
Marine <marinemontailler@gmail.com>
2020-11-18 11:58:15 +0100

    add link in programme header

--- FRONT/gatsby-sens-interdits/src/components/specifics/Header/Ticketing.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Header/Ticketing.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default function Ticketing() {
+  const url = "http://localhost:8000"
   return (
     <div className="billetterie-programme">
       <a href="https://www.sensinterdits.org" title="billetterie" className="highlight to-uppercase">Billetterie</a>
-      <a href="https://www.sensinterdits.org" title="programme" className="highlight to-uppercase">Programme</a>
+      <a href={url + "/programme"} title="programme" className="highlight to-uppercase">Programme</a>
     </div>
   )
 }
