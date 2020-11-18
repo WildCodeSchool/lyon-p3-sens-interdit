889502af6002687f88037a37b05c30a5b52ebaae
Marine <marinemontailler@gmail.com>
2020-11-18 12:04:53 +0100

    anchor to newsletter in header-footer

--- FRONT/gatsby-sens-interdits/src/components/specifics/Header/NewsletterBtn.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Header/NewsletterBtn.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function NewsletterBtn() {
     return (
-        <div className="highlight newsletter-header to-uppercase">Newsletter</div>
+        <div className="highlight newsletter-header to-uppercase">
+            <a href="#newsletter" title="ancor to newsletter">Newsletter</a>
+        </div>
     )
 }
