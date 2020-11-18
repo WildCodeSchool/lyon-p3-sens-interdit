68a40930b3e58003388728d4f14e2734be0777ac
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-11-18 10:20:23 +0100

    dont know what is it but i need to rebase

--- FRONT/gatsby-sens-interdits/src/components/specifics/Programme/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Programme/index.js
@@ -7,6 +7,81 @@ import CalendarLarge from "../../globals/Calendar/CalendarLarge";
 import ImageCarousel from "../../globals/Carousel/ImageCarousel";
 
 export default function ProgrammePage() {
+
+  const props = {list: {}};
+  const [list, setList] = useState(props.list);
+
+  useEffect(() => {
+    setList(props.list);
+  }, [props]);
+
+  function countryFilter(e) {
+    e.preventDefault();
+    console.log("Le filtre pays a été cliqué.");
+  }
+
+  function authorFilter(e) {
+    e.preventDefault();
+    console.log("Le filtre metteur en scene a été cliqué.");
+  }
+
+  function placeFilter(e) {
+    e.preventDefault();
+    console.log("Le filtre lieu a été cliqué.");
+  }
+  function resetFilter(e) {
+    e.preventDefault();
+    console.log("Les filtres sont reinitialisés.");
+  }
+
+  const affichageList = () => {
+    console.log("AAAAAAAAAAAAAAAA", "list");
+    if (list.length === 0) {
+      return (
+        <img
+          src="https://media.giphy.com/media/WiIuC6fAOoXD2/giphy.gif"
+          alt="loading"
+        />
+      );
+    } else {
+      return list.map(spectacle => {
+        console.log(spectacle.all_photos.photo_1);
+        return (
+          <Thumbnail
+            key={
+              spectacle.titre +
+              Math.floor(Math.random() * 10) +
+              Math.floor(Math.random() * 10)
+            }
+            affiche={
+              spectacle.all_photos.photo_1 !== undefined
+                ? spectacle.all_photos.photo_1
+                : photoTest
+            }
+            date={
+              spectacle.info_pratique.dates === undefined ||
+              spectacle.info_pratique.dates.date_1 === undefined
+                ? "inconnu"
+                : spectacle.info_pratique.dates.date_1
+            }
+            country={
+              spectacle.credits === undefined ||
+              spectacle.credits.pays === undefined
+                ? "inconnu"
+                : spectacle.credits.pays
+            }
+            name={spectacle.titre}
+            team={
+              spectacle.credits === undefined ||
+              spectacle.credits.auteur === undefined
+                ? "inconnu"
+                : spectacle.credits.auteur
+            }
+          />
+        );
+      });
+    }
+  };
   return (
     <div className="global-programme-page">
       <ImageCarousel />
