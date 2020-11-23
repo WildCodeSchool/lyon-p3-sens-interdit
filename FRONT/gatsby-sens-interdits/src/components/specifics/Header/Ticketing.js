0656239e2df9a3c053d696eda76ff237199abfda
ShivanPaishari <shivan.paishari@yahoo.com>
2020-11-23 15:48:33 +0100

    adding mapado main link

--- FRONT/gatsby-sens-interdits/src/components/specifics/Header/Ticketing.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Header/Ticketing.js
@@ -4,7 +4,7 @@ export default function Ticketing() {
   const url = "http://localhost:8000"
   return (
     <div className="billetterie-programme">
-      <a href="https://www.sensinterdits.org" title="billetterie" className="highlight to-uppercase">Billetterie</a>
+      <a href="https://sensinterdits.mapado.com/" title="billetterie" className="highlight to-uppercase" target="_blank" rel="noreferrer">Billetterie</a>
       <a href={url + "/programme"} title="programme" className="highlight to-uppercase">Programme</a>
     </div>
   )
