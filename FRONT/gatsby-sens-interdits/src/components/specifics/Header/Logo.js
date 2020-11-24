ba2c2fa4069fd62c4c27846beb9659623d908dae
Marine <marinemontailler@gmail.com>
2020-11-24 16:45:45 +0100

    comment graphql logo

--- FRONT/gatsby-sens-interdits/src/components/specifics/Header/Logo.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Header/Logo.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logosens from '../../../assets/img/logoheader.png';
-import { graphql, useStaticQuery } from "gatsby";
+// import { graphql, useStaticQuery } from "gatsby";
 
 
 export default function Logo() {
-
-
-    const { strapiLogoPrincipal } = useStaticQuery(graphql`
-    query MyQueryLogo {
-        strapiLogoPrincipal {
-          Logo {
-            id
-          }
-        }
-      }
-      `);
+    // const { strapiLogoPrincipal } = useStaticQuery(graphql`
+    // query MyQueryLogo {
+    //     strapiLogoPrincipal {
+    //       Logo {
+    //         id
+    //       }
+    //     }
+    //   }
+    //   `);
     return (
         <div className="main-logo">
             <a href="https://www.sensinterdits.org" title="site sens-interdits">
-                <img src={process.env.GATSBY_API_URL + strapiLogoPrincipal.Logo} alt={"logo du festival sens-interdits"} />
+                <img src={logosens} alt={"logo du festival sens-interdits"} />
             </a>
         </div>
     )
