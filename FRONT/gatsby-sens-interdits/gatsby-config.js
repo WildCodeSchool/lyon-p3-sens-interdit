ea5698ff9b49f573d5eb7366cb1e713b73c0f591
Marine <marinemontailler@gmail.com>
2020-11-24 10:02:08 +0100

    put the url in images carousel and homepage

--- FRONT/gatsby-sens-interdits/gatsby-config.js
+++ FRONT/gatsby-sens-interdits/gatsby-config.js
@@ -10,11 +10,7 @@ module.exports = {
         apiURL: process.env.API_URL || "http://localhost:1337",
         queryLimit: -1, // -1 = No limits // Default to 100
         contentTypes: [`festival`, `spectacle`, `transmission`],
-<<<<<<< HEAD
         singleTypes: [`global-menu`, `home-display-tab`, `hors-scene-page`]
-=======
-        singleTypes: [`global-menu`, `home-display-tab`,`hors-scene-page`],
->>>>>>> d242a72779064473bf7dae2eee3ea1afba464cc1
       },
     },
   ],
