f9f4cbb07addca87144ac6cdc6a2213801d2d8f0
Marine <marinemontailler@gmail.com>
2020-11-24 12:02:18 +0100

    association page display tabsystem with hors scene component

---
+++ FRONT/gatsby-sens-interdits/src/pages/associations.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AssociationPage from "../components/specifics/Association";
+
+const Associations = () => {
+    return <AssociationPage />;
+};
+
+export default Associations;
