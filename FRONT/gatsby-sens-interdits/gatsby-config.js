64aad8e3d8a2a00ec5fdc94ac8685c5b38fd13dd
Seblecaribou <morin.seb.89@gmail.com>
2020-11-25 11:41:54 +0100

    Changed stuff for graphql

--- FRONT/gatsby-sens-interdits/gatsby-config.js
+++ FRONT/gatsby-sens-interdits/gatsby-config.js
@@ -7,10 +7,19 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.GATSBY_API_URL || "http://localhost:1337",
         queryLimit: -1, // -1 = No limits // Default to 100
-        contentTypes: [`festival`, `spectacle`, `transmission`],
-        singleTypes: [`global-menu`, `home-display-tab`,`hors-scene-page`],
+        contentTypes: [`festival`, `spectacle`, `archives-old`],
+        singleTypes: [
+          `global-menu`,
+          `home-display-tab`,
+          `hors-scene-page`,
+          `logo-principal`,
+        ],
+      },
+      loginData: {
+        identifier: "",
+        password: "",
       },
     },
   ],
