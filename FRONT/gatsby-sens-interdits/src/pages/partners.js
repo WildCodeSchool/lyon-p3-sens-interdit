131dc1f65ab6f0e8b48add749bbeebd51df305fe
Justine <hell.justine@gmail.com>
2020-11-25 15:56:48 +0100

    WIP partner page - no query yet (hard coded data)

---
+++ FRONT/gatsby-sens-interdits/src/pages/partners.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PartnersPage from "../components/specifics/Partners";
+
+export default function Programme() {
+  return (
+    <>
+      <PartnersPage />
+    </>
+  );
+}
