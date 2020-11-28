e83865595af9fb33418cfbe4a7808268951ab1e0
faure <grldfaure@gmail.com>
2020-11-28 23:45:06 +0100

    generate  all oldarchives page and make link

---
+++ FRONT/gatsby-sens-interdits/src/templates/archiveSpectacle.js
@@ -0,0 +1,31 @@
+import { graphql } from "gatsby";
+import React from "react";
+import "./Index.css";
+
+export default function ArchiveSpectaclePage({ data }) {
+
+  return (
+    <div className="global-spectacle-page">
+      <p>{data.strapiArchivesOld.titre}</p>
+      <p>{data.strapiArchivesOld.presentation}</p>
+    </div>
+  );
+}
+
+// This query needs to be dynamic based on the id of the spectacle
+// (example: id="test-spectacle" --> the route will be: http://localhost:8000/spectacle/test-spectacle/
+export const query = graphql`
+query MyQueryArchiveDeux($id: String!) {
+  strapiArchivesOld(id: {eq: $id}) {
+    id
+    titre
+    duree
+    pays
+    lieu
+    presentation
+    tableElementArchiveOld{
+    content
+    }
+  }
+}
+`
