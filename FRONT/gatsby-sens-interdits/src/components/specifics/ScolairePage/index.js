87863af7726f5820b60488f6626d16985c6f8282
Seblecaribou <morin.seb.89@gmail.com>
2020-11-30 12:14:28 +0100

    Working properly

--- FRONT/gatsby-sens-interdits/src/components/specifics/ScolairePage/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/ScolairePage/index.js
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Index.css";
-import TabSystemH from "../../globals/TabSystems/TabSystemH";
 
 export default function ScolairePage(props) {
-  return (
-    <div>
-      <img src="" alt="" />
-      <p></p>
-      <TabSystemH />
-    </div>
-  );
+  return <p>Hello</p>;
 }
--- FRONT/gatsby-sens-interdits/src/pages/scolaires.js
+++ FRONT/gatsby-sens-interdits/src/pages/scolaires.js
@@ -3,11 +3,22 @@ import React from "react";
 import ScolairePage from "../components/specifics/ScolairePage";
 
 export default function Scolaire() {
-  //   const data = useStaticQuery(graphql`
-  //   query{
+  const data = useStaticQuery(graphql`
+    query scolaireQuery {
+      strapiScolaire {
+        id
+        image {
+          url
+          id
+        }
+        tab_element {
+          title
+          content
+        }
+        title
+      }
+    }
+  `);
 
-  //   }
-  //   `);
-
-  return <ScolairePage data="" />;
+  return <ScolairePage data={data.strapiScolaire} />;
 }
