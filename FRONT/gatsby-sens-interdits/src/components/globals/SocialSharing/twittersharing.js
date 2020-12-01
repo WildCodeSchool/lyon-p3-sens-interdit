669e7b3b528c400f604548fadf8666228d432a53
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-12-01 10:22:01 +0100

    recovering previous work

---
+++ FRONT/gatsby-sens-interdits/src/components/globals/SocialSharing/twittersharing.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import twitterIco from './ico/PICTO SI TWITTER.svg';
+
+function Twitter() {
+	return (
+		<div className='twitwrapper'>
+			<a
+				href='https://twitter.com/share?ref_src=twsrc%5Etfw'
+				class='twitter-share-button'
+				data-show-count='false'>
+				<img src={twitterIco} alt='twitter' width="50px" height="50px" />
+			</a>
+			<script
+				async
+				src='https://platform.twitter.com/widgets.js'
+				charset='utf-8'></script>
+
+		</div>
+	);
+}
+export default Twitter;
