e7e14a8f048fe83904a150a6ff54c81d9f7f1df0
Seblecaribou <morin.seb.89@gmail.com>
2020-12-02 21:47:59 +0100

    Added english version to "paroles d'exil" page

--- FRONT/gatsby-sens-interdits/src/pages/parole.js
+++ FRONT/gatsby-sens-interdits/src/pages/parole.js
@@ -1,8 +1,10 @@
 import { graphql, useStaticQuery } from "gatsby";
-import React from "react";
+import React, { useContext } from "react";
+import LanguageContext from "../components/context/LanguageContext";
 import GeneriquePage from "../components/specifics/Generique";
 
 export default function Parole() {
+  const { language } = useContext(LanguageContext);
   const data = useStaticQuery(graphql`
     query paroleQuery {
       strapiParole {
@@ -15,8 +17,14 @@ export default function Parole() {
           title
           content
         }
+        tab_element_en {
+          title
+          content
+        }
         title
+        title_en
         description
+        description_en
       }
     }
   `);
@@ -24,9 +32,19 @@ export default function Parole() {
   return (
     <GeneriquePage
       image={data.strapiParole.image[0].url}
-      title={data.strapiParole.title}
-      description={data.strapiParole.description}
-      tab_element={data.strapiParole.tab_element}
+      title={
+        language === "fr" ? data.strapiParole.title : data.strapiParole.title_en
+      }
+      description={
+        language === "fr"
+          ? data.strapiParole.description
+          : data.strapiParole.description_en
+      }
+      tab_element={
+        language === "fr"
+          ? data.strapiParole.tab_element
+          : data.strapiParole.tab_element_en
+      }
     />
   );
 }
