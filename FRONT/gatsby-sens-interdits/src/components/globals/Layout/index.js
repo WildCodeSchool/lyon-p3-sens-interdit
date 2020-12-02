3e5fc0d201e168b53873e12f22ab5b79a7010f2e
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-12-02 15:57:13 +0100

    blabla commit blabla

--- FRONT/gatsby-sens-interdits/src/components/globals/Layout/index.js
+++ FRONT/gatsby-sens-interdits/src/components/globals/Layout/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import Footer from "../../specifics/Footer"
 import Header from "../../specifics/Header"
 import "../../../assets/styles/global.css"
+import SharingBox from "./../SocialSharing/SharingBox";
 
 export default function Index({ children }) {
   return (
     <>
       <Header />
+      <SharingBox />
       <main>{children}</main>
       <Footer />
     </>
