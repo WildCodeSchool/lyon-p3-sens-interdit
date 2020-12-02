f6497c5c100f553704730fe899f9b5be150404e7
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-12-02 09:51:25 +0100

    waiting for working css

--- FRONT/gatsby-sens-interdits/src/components/specifics/Search/Result.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Search/Result.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Result.css";
+
 export default function Result(props) {
   return (
-    <div class="flex-wrapper">
+    <div className="flex-wrapper">
       <img src={props.image} alt={props.id} />
-      <div class="text-wrapper">
+      <div className="text-wrapper">
         <h2>{props.title}</h2>
         <p>{props.description}</p>
       </div>
--- FRONT/gatsby-sens-interdits/src/components/specifics/Search/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Search/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Result from "./Result";
+import fakeData from "./fakeData.json";
 import { graphql, useStaticQuery } from "gatsby";
 export default function ResultPage() {
-  const data = useStaticQuery(graphql``);
-  return;
-  <>
-        <h2>Résultats de la recherche:</h2>
-        
-    {data.map(item => {
-      <Result
-        title={data.title}
-        image={data.url}
-        description={data.description}
-      />;
-    })}
-      
-  </>;
+  const data = fakeData;
+  return (
+    <>
+          <h2>Résultats de la recherche:</h2>
+          
+      {data.map(item => (
+        <Result
+          key={item.id}
+          title={item.title}
+          image={item.url}
+          description={item.description}
+        />
+      ))}
+        
+    </>
+  );
 }
