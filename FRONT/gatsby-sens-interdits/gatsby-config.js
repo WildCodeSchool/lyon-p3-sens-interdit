69404b7c3dd3bcce6c634f8491844296ff12ff76
shanti <shivanpaishari@gmail.com>
2020-12-04 15:40:42 +0100

    icestatus for seo. only component with helmet in pages

--- FRONT/gatsby-sens-interdits/gatsby-config.js
+++ FRONT/gatsby-sens-interdits/gatsby-config.js
@@ -8,35 +8,37 @@ module.exports = {
     image:"",
     twitterUsername: "@sens-interdits",
   },
-  plugins: [
-    {
-      resolve: `gatsby-source-strapi`,
-      options: {
-        apiURL: process.env.GATSBY_API_URL,
-        queryLimit: -1, // -1 = No limits // Default to 100
-        contentTypes: [`festival`, `spectacle`,`archives-old`, `type-of-event`,`articlecontent`, `typeofarticle`],
-        singleTypes: [
-          `global-menu`,
-          `logo-principal`,
-          `transmission`,
-          `assopage`,
-          `homepage`,
-          `partnerpage`,
-          `ecole`,
-          `webradio`,
-          `scolaire`,
-          `seminaire`,
-          `parole`,
-          `home-display-tab`,
-          `partnerpage`,
-          `newspage`,
-          `newstab`,
-          `hors-scene-page`,
-          `hors-scene-tab`,
-          `archive-description`,
-          `diffusion-production`
-        ],
+  plugins: 
+    [`gatsby-plugin-react-helmet`],
+    [
+      {
+        resolve: `gatsby-source-strapi`,
+        options: {
+          apiURL: process.env.GATSBY_API_URL,
+          queryLimit: -1, // -1 = No limits // Default to 100
+          contentTypes: [`festival`, `spectacle`,`archives-old`, `type-of-event`,`articlecontent`, `typeofarticle`],
+          singleTypes: [
+            `global-menu`,
+            `logo-principal`,
+            `transmission`,
+            `assopage`,
+            `homepage`,
+            `partnerpage`,
+            `ecole`,
+            `webradio`,
+            `scolaire`,
+            `seminaire`,
+            `parole`,
+            `home-display-tab`,
+            `partnerpage`,
+            `newspage`,
+            `newstab`,
+            `hors-scene-page`,
+            `hors-scene-tab`,
+            `archive-description`,
+            `diffusion-production`
+          ],
+        },
       },
-    },
-  ],
+    ],
 };
