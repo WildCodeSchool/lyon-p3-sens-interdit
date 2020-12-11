e1801fede4efc90b427a9ff84f9580c219878c92
shanti <shivanpaishari@gmail.com>
2020-12-11 14:02:36 +0100

    adding seo in scolaire

--- FRONT/gatsby-sens-interdits/src/pages/scolaires.js
+++ FRONT/gatsby-sens-interdits/src/pages/scolaires.js
@@ -2,9 +2,11 @@ import React, { useContext } from "react";
 import LanguageContext from "../components/context/LanguageContext";
 import { graphql, useStaticQuery } from "gatsby";
 import GeneriquePage from "../components/specifics/Generique";
+import SEO from "../components/SEO/seo";
+
 
 export default function Scolaire() {
-  const { checkEnContext } = useContext(LanguageContext);
+  const { checkEnContext, LANG} = useContext(LanguageContext);
 
   const { strapiScolaire } = useStaticQuery(graphql`
     query scolaireQuery {
@@ -34,25 +36,48 @@ export default function Scolaire() {
         title_en
         description
         description_en
+        seo_scolaire {
+          description
+          description_en
+          image {
+            url
+          }
+          image_en {
+            url
+          }
+          title
+          title_en
+        }
       }
     }
   `);
 
+  let seo = strapiScolaire.seo_scolaire;
+  const title = LANG === 'en' ?  seo.title_en : seo.title;
+  const description = LANG === 'en' ? seo.description_en: seo.description;
+  const image = LANG === 'en' ? seo.image[0].url_en : seo.image[0].url;
+console.log ({seo})
+
   return (
-    <GeneriquePage
-      image={strapiScolaire.image[0].url}
-      title={checkEnContext(
-          strapiScolaire.title,
-        strapiScolaire.title_en
-      )}
-      description={checkEnContext(
-        strapiScolaire.description,
-        strapiScolaire.description_en
-      )}
-      tab_element={checkEnContext(
-        strapiScolaire.tab_element,
-        strapiScolaire.tab_element_en
-      )}
-    />
+    <>
+      <SEO title={title !== undefined ? title : checkEnContext(strapiScolaire.title, strapiScolaire.title_en)} 
+        description={description !== undefined ? description : ""}  
+        image={image !== undefined ? image : strapiScolaire.image[0].url} />
+      <GeneriquePage
+        image={strapiScolaire.image[0].url}
+        title={checkEnContext(
+            strapiScolaire.title,
+          strapiScolaire.title_en
+        )}
+        description={checkEnContext(
+          strapiScolaire.description,
+          strapiScolaire.description_en
+        )}
+        tab_element={checkEnContext(
+          strapiScolaire.tab_element,
+          strapiScolaire.tab_element_en
+        )}
+      />
+    </>
   );
 }
