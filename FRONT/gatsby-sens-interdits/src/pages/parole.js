96b40ffe654f890ce79b4952722b73316d88ae87
shanti <shivanpaishari@gmail.com>
2020-12-11 14:40:17 +0100

    adding seo in parole page

--- FRONT/gatsby-sens-interdits/src/pages/parole.js
+++ FRONT/gatsby-sens-interdits/src/pages/parole.js
@@ -2,9 +2,10 @@ import { graphql, useStaticQuery } from "gatsby";
 import React, { useState, useEffect, useContext } from "react";
 import LanguageContext from "../components/context/LanguageContext";
 import GeneriquePage from "../components/specifics/Generique";
+import SEO from "../components/SEO/seo";
 
 export default function Parole() {
-  const { checkEnContext } = useContext(LanguageContext);
+  const { checkEnContext, LANG } = useContext(LanguageContext);
 
   const data = useStaticQuery(graphql`
     query paroleQuery {
@@ -28,25 +29,48 @@ export default function Parole() {
         title_en
         description
         description_en
+        seo_parole {
+          description
+          description_en
+          image {
+            url
+          }
+          image_en {
+            url
+          }
+          title
+          title_en
+        }
       }
     }
   `);
 
+  let seo = data.strapiParole.seo_parole;
+  const title = LANG === 'en' ?  seo.title_en : seo.title;
+  const description = LANG === 'en' ? seo.description_en: seo.description;
+  const image = LANG === 'en' ? seo.image[0].url_en : seo.image[0].url;
+console.log({description})
+
   return (
-    <GeneriquePage
-      image={data.strapiParole.image[0].url}
-      title={checkEnContext(
-        data.strapiParole.title,
-        data.strapiParole.title_en
-      )}
-      description={checkEnContext(
-        data.strapiParole.description,
-        data.strapiParole.description_en
-      )}
-      tab_element={checkEnContext(
-        data.strapiParole.tab_element,
-        data.strapiParole.tab_element_en
-      )}
-    />
+    <>
+      <SEO title={title !== undefined ? title : checkEnContext(data.strapiParole.title, data.strapiParole.title_en)} 
+        description={description !== undefined ? description : ""}  
+        image={image !== undefined ? image : data.strapiParole.image[0].url} />
+      <GeneriquePage
+        image={data.strapiParole.image[0].url}
+        title={checkEnContext(
+          data.strapiParole.title,
+          data.strapiParole.title_en
+        )}
+        description={checkEnContext(
+          data.strapiParole.description,
+          data.strapiParole.description_en
+        )}
+        tab_element={checkEnContext(
+          data.strapiParole.tab_element,
+          data.strapiParole.tab_element_en
+        )}
+      />
+    </>
   );
 }
