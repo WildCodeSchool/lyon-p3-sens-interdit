601cda17cf69dcac678fa28607dc46b68a8617ba
Marine <marinemontailler@gmail.com>
2020-12-11 16:31:05 +0100

    fixe size redsquare in carousel height

--- FRONT/gatsby-sens-interdits/src/components/globals/Carousel/RedTitleCardOld.js
+++ FRONT/gatsby-sens-interdits/src/components/globals/Carousel/RedTitleCardOld.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./RedTitleCard.css";
 
-function RedTitleCardOld({ title, displayed, booking}) {
+function RedTitleCardOld({ title, displayed }) {
 
   return displayed ? (
     <>
       <div className="red"></div>
-      <div className="image-text image-text-old">
-    
+      <div className="image-text">
         <p >{title}</p>
       </div>
-      
+
     </>
   ) : (
-    <></>
-  );
+      <></>
+    );
 }
 
 export default RedTitleCardOld;
