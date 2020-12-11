6e1dab6dde6160006dc23823bc262ec31906b944
shanti <shivanpaishari@gmail.com>
2020-12-11 15:32:50 +0100

    adding seo in newspage

--- FRONT/gatsby-sens-interdits/src/components/specifics/NewsPage/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/NewsPage/index.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { graphql, useStaticQuery } from "gatsby";
+import SEO from "../../../components/SEO/seo";
 import TabSystemH from "../../globals/TabSystems/TabSystemH";
 import ImageCarousel from "../../globals/Carousel/ImageCarousel";
 import "../../../assets/styles/global.css";
@@ -7,7 +8,7 @@ import "../../../templates/article.css";
 import LanguageContext from "../../context/LanguageContext";
 
 export default function NewsPage() {
-  const { checkEnContext } = useContext(LanguageContext);
+  const { checkEnContext, LANG} = useContext(LanguageContext);
   const strapiNewsQuery = useStaticQuery(graphql`
     query strapiNewsQuery {
       allStrapiNewspage {
@@ -17,6 +18,18 @@ export default function NewsPage() {
           id
           title
           title_en
+          seo_newspage {
+            description
+            description_en
+            image {
+              url
+            }
+            image_en {
+              url
+            }
+            title
+            title_en
+          }
         }
       }
       allStrapiArticlecontent {
@@ -57,29 +70,39 @@ export default function NewsPage() {
   const newsArticlesQuery = strapiNewsQuery.allStrapiArticlecontent.nodes;
   const textOverFlow = true;
   const linkStatus = true;
+  let seo = newsPageQuery.seo_newspage;
+  const title = LANG === 'en' ?  seo.title_en : seo.title;
+  const description = LANG === 'en' ? seo.description_en : seo.description;
+  const image = LANG === 'en' ? seo.image[0].url_en : seo.image[0].url;
 
   return (
-    <div>
-      <ImageCarousel />
-      <div className="container">
-        <div className="red-arrow"></div>
-        <div>
-          <h3 className="to-uppercase">
-            {checkEnContext(newsPageQuery.title, newsPageQuery.title_en)}
-          </h3>
-          <p>
-            {checkEnContext(newsPageQuery.content, newsPageQuery.content_en)}
-          </p>
-        </div>
-        <div>
-          <TabSystemH
-            tabContent={newsTabQuery.newstab}
-            articles={newsArticlesQuery}
-            textOverFlow={textOverFlow}
-            linkStatus={linkStatus}
-          />
+    <>
+      <SEO 
+        title={title !== undefined ? title : newsPageQuery["title" + LANG]} 
+        description={description !== undefined ? description : newsPageQuery["description" + LANG] } 
+        image={image !== undefined ? image : newsTabQuery.picturetop[0].url} />
+      <div>
+        <ImageCarousel />
+        <div className="container">
+          <div className="red-arrow"></div>
+          <div>
+            <h3 className="to-uppercase">
+              {checkEnContext(newsPageQuery.title, newsPageQuery.title_en)}
+            </h3>
+            <p>
+              {checkEnContext(newsPageQuery.content, newsPageQuery.content_en)}
+            </p>
+          </div>
+          <div>
+            <TabSystemH
+              tabContent={newsTabQuery.newstab}
+              articles={newsArticlesQuery}
+              textOverFlow={textOverFlow}
+              linkStatus={linkStatus}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
