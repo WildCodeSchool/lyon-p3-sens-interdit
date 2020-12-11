898c0c72e83842887847b8e9b747ea6dd676f295
Maxence <maxence.meloni@wildcodeschool.com>
2020-12-11 16:34:08 +0100

    add ignore warning css on webpack compilation

--- FRONT/gatsby-sens-interdits/gatsby-node.js
+++ FRONT/gatsby-sens-interdits/gatsby-node.js
@@ -273,3 +273,16 @@ exports.createPages = async params => {
     turnFestivalIntoArchiveFestivalPages(params),
   ]);
 };
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+        plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
