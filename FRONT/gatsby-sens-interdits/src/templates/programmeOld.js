af29acb070aaab3c98378fee5fa83e4920c3a765
Justine <hell.justine@gmail.com>
2020-12-13 21:41:38 +0100

    archive programme text justify and padding

--- FRONT/gatsby-sens-interdits/src/templates/programmeOld.js
+++ FRONT/gatsby-sens-interdits/src/templates/programmeOld.js
@@ -1,56 +1,60 @@
 import React from "react";
 import ThumbnailOldArchive from "../components/globals/ThumbnailOldArchive";
-import { graphql,Link } from "gatsby";
-import Playicon from "../assets/img/play_icon.png"
+import { graphql, Link } from "gatsby";
+import Playicon from "../assets/img/play_icon.png";
 import "./archiveFestival.css";
 
 function Programme({ data }) {
-    const archive = data.allStrapiFestivalarchive.nodes[0];
-    if (archive !== undefined) {
-        return (
-            <>
-                <div className="global-margin archive-global-styling">
-                    <div className="image-generique-page">
-                        {archive.poster[0] !== undefined && archive.poster[0] ?
-                            <img
-                                src={
-                                    process.env.GATSBY_API_URL +
-                                    archive.poster[0].url
-                                }
-                                alt={archive.title}
-                            />
-                            : null}
-                    </div>
-                    <h1 className="to-uppercase">
-                        Edition {archive !== undefined ? archive.year : null} -{" "}
-                        <span>{archive !== undefined ? archive.title : null}</span>
-                        {archive.download[0] !== undefined ?
-                            <a href={process.env.GATSBY_API_URL + archive.download[0].url}
-                               title="download programme"
-                               download>
-            <span className="downloadPdf">
-                <img src={Playicon} alt="bouton telechargement" width="30"/>Télécharger le programme</span>
-                            </a> : null}
-                    </h1>
-                    <div>{archive !== undefined ? archive.description : null}</div>
-                    <div className="archive-transmission-grid-wrapper">
-                        {data.allStrapiArchivesOld.nodes.map(elem => (
-                            <ThumbnailOldArchive
-                                id={elem.strapiId}
-                                key={elem.id}
-                                country={elem.pays}
-                                name={elem.titre}
-                                team={elem.credits_2}
-                                affiche={`${process.env.GATSBY_IMAGE_URL}` + elem.photo_1}
-                            />
-                        ))}
-                    </div>
-                </div>
-            </>
-        );
-    } else {
-        return(<></>);
-    }
+  const archive = data.allStrapiFestivalarchive.nodes[0];
+  if (archive !== undefined) {
+    return (
+      <>
+        <div className="global-margin archive-global-styling">
+          <div className="image-generique-page">
+            {archive.poster[0] !== undefined && archive.poster[0] ? (
+              <img
+                src={process.env.GATSBY_API_URL + archive.poster[0].url}
+                alt={archive.title}
+              />
+            ) : null}
+          </div>
+          <h1 className="to-uppercase">
+            Edition {archive !== undefined ? archive.year : null} -{" "}
+            <span>{archive !== undefined ? archive.title : null}</span>
+            {archive.download[0] !== undefined ? (
+              <a
+                href={process.env.GATSBY_API_URL + archive.download[0].url}
+                title="download programme"
+                download
+              >
+                <span className="downloadPdf">
+                  <img src={Playicon} alt="bouton telechargement" width="30" />
+                  Télécharger le programme
+                </span>
+              </a>
+            ) : null}
+          </h1>
+          <div className="text-justify">
+            {archive !== undefined ? archive.description : null}
+          </div>
+          <div className="archive-transmission-grid-wrapper">
+            {data.allStrapiArchivesOld.nodes.map(elem => (
+              <ThumbnailOldArchive
+                id={elem.strapiId}
+                key={elem.id}
+                country={elem.pays}
+                name={elem.titre}
+                team={elem.credits_2}
+                affiche={`${process.env.GATSBY_IMAGE_URL}` + elem.photo_1}
+              />
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return <></>;
+  }
 }
 
 export default Programme;
