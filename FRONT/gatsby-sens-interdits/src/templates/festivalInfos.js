1a3919fc09c6201f59db6b62c2ee19183296c24e
Justine <hell.justine@gmail.com>
2020-12-13 20:11:59 +0100

    add translation for festival info and places

--- FRONT/gatsby-sens-interdits/src/templates/festivalInfos.js
+++ FRONT/gatsby-sens-interdits/src/templates/festivalInfos.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { graphql } from "gatsby";
+import LanguageContext from "../components/context/LanguageContext";
 
 import GeneriquePage from "../components/specifics/Generique";
 
 export default function FestivalInfosPage({ data }) {
+  const { checkEnContext } = useContext(LanguageContext);
+
   const festivalInfo = data.festivalInfo;
 
   return (
     <div>
       <GeneriquePage
-        image={festivalInfo.images !== undefined && festivalInfo.image[0].url}
+        image={festivalInfo.image !== undefined && festivalInfo.image[0].url}
         title="Informations Pratiques"
-        description={festivalInfo.description}
-        tab_element={festivalInfo.tab_element}
+        description={checkEnContext(
+          festivalInfo.description,
+          festivalInfo.description_en
+        )}
+        tab_element={checkEnContext(
+          festivalInfo.tab_element,
+          festivalInfo.tab_element_en
+        )}
       />
     </div>
   );
@@ -24,12 +33,26 @@ export const query = graphql`
       id
       strapiId
       description
+      description_en
       image {
         url
         name
         id
       }
       tab_element {
+        content
+        id
+        title
+        logo {
+          id
+          url
+          image {
+            url
+            id
+          }
+        }
+      }
+      tab_element_en {
         content
         id
         title
