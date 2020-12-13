88eafa66bbba2d3917fd95790977559f22682d74
Marine <marinemontailler@gmail.com>
2020-12-13 19:37:37 +0100

    add title on each Link in sitemap page

--- FRONT/gatsby-sens-interdits/src/components/specifics/Sitemap/index.js
+++ FRONT/gatsby-sens-interdits/src/components/specifics/Sitemap/index.js
@@ -18,7 +18,7 @@ export default function SitemapPage() {
                 <h1 className='to-uppercase'>Sitemap</h1>
                 <div className='sitemap-list'>
                     <ul>
-                        <Link to="/">
+                        <Link to="/" title="vers la page d'accueil">
                             <h1>{checkEnContext(
                                 "Accueil",
                                 "Home"
@@ -27,37 +27,37 @@ export default function SitemapPage() {
                     </ul>
                     <ul>
                         <li><Link
-                            to={`/festival/${currentFestivalTitle}_${currentFestivalStrapiId}`}>
+                            to={`/festival/${currentFestivalTitle}_${currentFestivalStrapiId}`} title="vers la page du festival sens-interdits">
                             <h1>{checkEnContext(
                                 "Festival",
                                 "Festival"
                             )}</h1>
                         </Link></li>
-                        <li><Link to='/programme'>{checkEnContext(
+                        <li><Link to='/programme' title="vers la page du programme en cours">{checkEnContext(
                             "Programme",
                             "Program"
                         )}</Link></li>
-                        <li><Link to='/hors-scene'>{checkEnContext(
+                        <li><Link to='/hors-scene' title="vers la page des évènements hors-scène">{checkEnContext(
                             "Hors-scène",
                             "Off scene"
                         )}</Link></li>
-                        <li><Link to='/ecole'>{checkEnContext(
+                        <li><Link to='/ecole' title="vers la page des écoles">{checkEnContext(
                             "Ecole",
                             "Academic"
                         )}</Link></li>
-                        <li><Link to="https://sensinterdits.mapado.com/">{checkEnContext(
+                        <li><Link to="https://sensinterdits.mapado.com/" title="vers la billetterie du festival">{checkEnContext(
                             "Billetterie",
                             "Ticket infos"
                         )}</Link></li>
                         <li><Link
-                            to={`/festival/${currentFestivalTitle}_${currentFestivalStrapiId}/infos`}>
+                            to={`/festival/${currentFestivalTitle}_${currentFestivalStrapiId}/infos`} title="vers les informations pratiques">
                             {checkEnContext(
                                 "Informations Pratiques",
                                 "Informations"
                             )}
                         </Link></li>
                         <li><Link
-                            to={`/festival/${currentFestivalTitle}_${currentFestivalStrapiId}/lieux`}>
+                            to={`/festival/${currentFestivalTitle}_${currentFestivalStrapiId}/lieux`} title="vers les lieux de représentations des spectacles">
                             {checkEnContext(
                                 "Lieux",
                                 "Places"
@@ -66,7 +66,7 @@ export default function SitemapPage() {
                     </ul>
                     <ul>
                         <li>
-                            <Link to="/diffusion-production" ><h1>
+                            <Link to="/diffusion-production" title="vers les diffusions et les productions"><h1>
                                 {checkEnContext(
                                     "Diffusion-Production",
                                     "Production"
@@ -74,11 +74,11 @@ export default function SitemapPage() {
                             </h1>
                             </Link>
                         </li>
-                        <li><Link to='/ecole'>{checkEnContext(
+                        <li><Link to='/ecole' title="vers les écoles">{checkEnContext(
                             "Ecole",
                             "Academic"
                         )}</Link></li>
-                        <li><Link to='/programme-tour'>{checkEnContext(
+                        <li><Link to='/programme-tour' title="vers le programme des spectacles en tournée">{checkEnContext(
                             "Spectacles en tournée",
                             "On tour"
                         )}</Link></li>
@@ -86,7 +86,7 @@ export default function SitemapPage() {
                     </ul>
 
                     <ul>
-                        <li><Link to="/transmissions"><h1>
+                        <li><Link to="/transmissions" title="vers la page transmissions"><h1>
                             {checkEnContext(
                                 "Ateliers-Transmission",
                                 "Transmission"
@@ -95,27 +95,27 @@ export default function SitemapPage() {
 
                         </Link></li>
                         <li><h2>Avec les publics</h2></li>
-                        <li><Link to='/webradio'>Webradio</Link></li>
-                        <li><Link to='/parole'>{checkEnContext(
+                        <li><Link to='/webradio' title="vers les webradios">Webradio</Link></li>
+                        <li><Link to='/parole' title="vers la page des paroles d'exiles">{checkEnContext(
                             "Paroles d'exil",
                             "Exile words"
                         )}</Link></li>
-                        <li><Link to='/scolaires'>{checkEnContext(
+                        <li><Link to='/scolaires' title="vers la page des scolaires">{checkEnContext(
                             "Scolaires",
                             "School"
                         )}</Link></li>
                         <li><h2>Avec les professionnels</h2></li>
-                        <li><Link to='/ecole'>{checkEnContext(
+                        <li><Link to='/ecole' title="vers la page des écoles">{checkEnContext(
                             "Ecole",
                             "Academic"
                         )}</Link></li>
-                        <li><Link to='/seminaires'>{checkEnContext(
+                        <li><Link to='/seminaires' title="vers la page des séminaires">{checkEnContext(
                             "Séminaire",
                             "Seminar"
                         )}</Link></li>
                     </ul>
                     <ul>
-                        <li><Link to="/association"><h1>
+                        <li><Link to="/association" title="vers la page de l'assocation"><h1>
                             {checkEnContext(
                                 "Association",
                                 "Association"
@@ -124,7 +124,7 @@ export default function SitemapPage() {
                         </Link></li>
                     </ul>
                     <ul>
-                        <li><Link to="/actualites">
+                        <li><Link to="/actualites" title="vers l'actualité de l'association">
                             <h1>{checkEnContext(
                                 "Actualités",
                                 "News"
@@ -133,7 +133,7 @@ export default function SitemapPage() {
                         </Link></li>
                     </ul>
                     <ul>
-                        <li><Link to="/archives"><h1>
+                        <li><Link to="/archives" title="vers les archives"><h1>
                             {checkEnContext(
                                 "Archives",
                                 "Archives"
